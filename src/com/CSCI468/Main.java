3d8db01df8339c4a9bea327680fa749452ced2b9
thechucklingatom <thechucklingatom@gmail.com>
2016-01-25 13:28:44 -0700

    added location strings for my laptop and my tower, added author javadoc tag.

--- src/com/CSCI468/Main.java
+++ src/com/CSCI468/Main.java
@@ -2,11 +2,18 @@ package com.CSCI468;
 
 /**
  * Created by Robert Putnam on 1/23/2016.
+ *
+ * @author Robert Putnam
  */
 public class Main {
     public static void main(String[] args){
         //generates antlr files to be used to parse
-        String[] arg0 = { "-visitor", "D:\\Documents\\Compilers\\res\\Grammars\\test.g4", "-package", "com.CSCI468.ANTLROut", "-o", "D:\\Documents\\Compilers\\src\\com\\CSCI468\\ANTLROut" };
+        //putTower location
+        String putTower = "D:\\Documents\\Compilers\\res\\Grammars\\test.g4";
+        //putLaptop location
+        String putLaptop = "C:\\Users\\robertp\\IdeaProjects\\Compilers\\res\\Grammars\\test.g4";
+
+        String[] arg0 = { "-visitor", putLaptop, "-package", "com.CSCI468.ANTLROut", "-o", "D:\\Documents\\Compilers\\src\\com\\CSCI468\\ANTLROut" };
         org.antlr.v4.Tool.main(arg0);
     }
 }
