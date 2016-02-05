67704bdd75cc0d8c3a0fb2bd775c5c5386808281
thechucklingatom <thechucklingatom@gmail.com>
2016-02-05 16:08:45 -0700

    Changed grammar file

--- src/com/CSCI468/Main.java
+++ src/com/CSCI468/Main.java
@@ -4,23 +4,31 @@ import com.CSCI468.ANTLROut.testParser;
 import org.antlr.v4.runtime.*;
 import com.CSCI468.ANTLROut.testLexer;
 
+import java.io.IOError;
+import java.io.IOException;
+
 /**
  * Created by Robert Putnam on 1/23/2016.
  *
  * @author Robert Putnam
  */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
 
         //uncomment if you change grammar, it ends the program so you can't have it if you want to check the parser
         //generateFiles();
 
+        ANTLRFileStream fileStream = new ANTLRFileStream("res/Step1/inputs/loop.micro");
         ANTLRInputStream in = new ANTLRInputStream("hello");
-        testLexer lexer = new testLexer(in);
+
+        testLexer lexer = new testLexer(fileStream);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         testParser parser = new testParser(tokens);
         parser.r();
-        System.out.println(tokens.get(0).getText());
+        parser.r();
+        for(int i = 0; i < tokens.size(); i++){
+            System.out.println(tokens.get(i));
+        }
     }
 
     public static void generateFiles(){
@@ -30,7 +38,7 @@ public class Main {
         //putLaptop location
         String putLaptop = "C:\\Users\\robertp\\IdeaProjects\\Compilers\\res\\Grammars\\test.g4";
 
-        String[] arg0 = { "-visitor", putTower, "-package", "com.CSCI468.ANTLROut", "-o", "D:\\Documents\\Compilers\\src\\com\\CSCI468\\ANTLROut" };
+        String[] arg0 = { "-visitor", putLaptop, "-package", "com.CSCI468.ANTLROut", "-o", "D:\\Documents\\Compilers\\src\\com\\CSCI468\\ANTLROut" };
         org.antlr.v4.Tool.main(arg0);
     }
 }
